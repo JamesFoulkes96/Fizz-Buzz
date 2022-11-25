d47bba582187dd11c959d85d198058e541ac2953
James Foulkes <118988296+JamesFoulkes96@users.noreply.github.com>
2022-11-25 15:50:44 +0000

    Add files via upload

---
+++ fizz-buzz-script-JF.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Fizz Buzz - James Foulkes - January 2022
+"""
+
+### FUNCTION 
+def Fizz_Buzz(f = 3, b = 5, n = 100):
+    '''prints integers from 1 to n
+    replacing multiples of f with "FIZZ",
+    multiples of b with "BUZZ" and 
+    multiples of both with "FIZZ-BUZZ!".'''
+    
+    for i in range(1, n + 1):
+        if i%(f*b) == 0:
+            print("FIZZ-BUZZ!")
+        elif i%f == 0:
+            print("FIZZ")
+        elif i%b == 0:
+            print("BUZZ")
+        else:
+            print(i)
+
+### Get inputs
+f = int(input("Fizz value: "))
+b = int(input("Buzz value: "))
+n = int(input("Upto: "))
+
+### Run function
+Fizz_Buzz(f, b, n)
